c12aaada93880f8fd0300a88697ecc38d54613da
programmer50th <78598373+programmer50th@users.noreply.github.com>
2021-12-17 15:06:59 +0800

    main function input

--- athlete/athlete.cpp
+++ athlete/athlete.cpp
@@ -2,5 +2,34 @@
 using namespace std;
 int main()
 {
-    cout << "abc" << endl;
+    int n;
+    cout << "Input the number of athletes:";
+    cin >> n;
+    Matrix m(n);
+    cout << "Input the weight M to F\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int temp;
+            cin >> temp;
+            m.container[i][j].weight = temp;
+        }
+    }
+    Matrix f(n);
+    cout << "Input the weight F to M\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int temp;
+            cin >> temp;
+            f.container[i][j].weight = temp;
+        }
+    }
+    m.dotMul(f);
+    m.print();
+    //todo
+
+    return 0;
 }
